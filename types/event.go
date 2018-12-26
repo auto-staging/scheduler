5149ff14185e671a297aa7ba24a09a80b2c5704b
Jan Ritter <jan-ritter@outlook.com>
2018-12-26 08:30:00 +0100

    docs: added go doc for types

--- types/event.go
+++ types/event.go
@@ -1,5 +1,6 @@
 package types
 
+// Event contains the event body used in the invokation of the Lambda
 type Event struct {
 	Repository string `json:"repository"`
 	Branch     string `json:"branch"`
--- types/status.go
+++ types/status.go
@@ -1,5 +1,6 @@
 package types
 
+// StatusUpdate struct is used for DynamoDB updates, because the update command requires all json keys to start with ":"
 type StatusUpdate struct {
 	Status string `json:":status"`
 }
