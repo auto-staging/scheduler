e0ed69c340879b60fc0503cd001c6051613b6ce9
Jan Ritter <jan-ritter@outlook.com>
2019-01-29 21:18:31 +0100

    test: added tests for changeEC2State function

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,12 +12,24 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+//
+// EC2 Tests
+//
+
 func TestChangeEC2StateStart(t *testing.T) {
-	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
-	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return([]*string{
+	instanceIDs := []*string{
 		aws.String("i-1234567890abcdef0"),
 		aws.String("i-1234567890abcdef1"),
-	}, nil)
+	}
+
+	cwEvent := types.Event{
+		Action:     "start",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+
+	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
+	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(instanceIDs, nil)
 
 	svcEC2HelperAPI.On("StartEC2Instances", mock.AnythingOfType("[]*string")).Return(nil)
 
@@ -28,17 +41,27 @@ func TestChangeEC2StateStart(t *testing.T) {
 		StatusHelperAPI: svcStatusHelperAPI,
 	}
 
-	err := base.changeEC2State(types.Event{Action: "start"})
+	err := base.changeEC2State(cwEvent)
 
 	assert.Nil(t, err, "Expected no error")
+	svcEC2HelperAPI.AssertCalled(t, "StartEC2Instances", instanceIDs)
+	svcStatusHelperAPI.AssertCalled(t, "SetStatusForEnvironment", cwEvent.Repository, cwEvent.Branch, "running")
 }
 
 func TestChangeEC2StateStop(t *testing.T) {
-	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
-	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return([]*string{
+	instanceIDs := []*string{
 		aws.String("i-1234567890abcdef0"),
 		aws.String("i-1234567890abcdef1"),
-	}, nil)
+	}
+
+	cwEvent := types.Event{
+		Action:     "stop",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+
+	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
+	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(instanceIDs, nil)
 
 	svcEC2HelperAPI.On("StopEC2Instances", mock.AnythingOfType("[]*string")).Return(nil)
 
@@ -50,12 +73,20 @@ func TestChangeEC2StateStop(t *testing.T) {
 		StatusHelperAPI: svcStatusHelperAPI,
 	}
 
-	err := base.changeEC2State(types.Event{Action: "stop"})
+	err := base.changeEC2State(cwEvent)
 
 	assert.Nil(t, err, "Expected no error")
+	svcEC2HelperAPI.AssertCalled(t, "StopEC2Instances", instanceIDs)
+	svcStatusHelperAPI.AssertCalled(t, "SetStatusForEnvironment", cwEvent.Repository, cwEvent.Branch, "stopped")
 }
 
 func TestChangeEC2StateNoInstances(t *testing.T) {
+	cwEvent := types.Event{
+		Action:     "stop",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+
 	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
 	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return([]*string{}, nil)
 
@@ -63,7 +94,147 @@ func TestChangeEC2StateNoInstances(t *testing.T) {
 		EC2HelperAPI: svcEC2HelperAPI,
 	}
 
-	err := base.changeEC2State(types.Event{})
+	err := base.changeEC2State(cwEvent)
 
 	assert.Nil(t, err, "Expected no error")
+	svcEC2HelperAPI.AssertCalled(t, "DescribeInstancesForTagsAndAction", cwEvent.Repository, cwEvent.Branch, cwEvent.Action)
+}
+
+func TestChangeEC2StateDescribeError(t *testing.T) {
+	errorMsg := errors.New("Test error")
+	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
+	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return([]*string{}, errorMsg)
+
+	base := services{
+		EC2HelperAPI: svcEC2HelperAPI,
+	}
+
+	err := base.changeEC2State(types.Event{})
+
+	assert.Error(t, err, "Expected error")
+	assert.Equal(t, errorMsg, err, "Error didn't match given error")
+}
+
+func TestChangeEC2StateStopError(t *testing.T) {
+	errorMsg := errors.New("Test error")
+
+	instanceIDs := []*string{
+		aws.String("i-1234567890abcdef0"),
+		aws.String("i-1234567890abcdef1"),
+	}
+
+	cwEvent := types.Event{
+		Action:     "stop",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+
+	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
+	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(instanceIDs, nil)
+
+	svcEC2HelperAPI.On("StopEC2Instances", mock.AnythingOfType("[]*string")).Return(errorMsg)
+
+	base := services{
+		EC2HelperAPI: svcEC2HelperAPI,
+	}
+
+	err := base.changeEC2State(cwEvent)
+
+	assert.Error(t, err, "Expected error")
+	assert.Equal(t, errorMsg, err, "Error didn't match given error")
+}
+
+func TestChangeEC2StateStopStatusError(t *testing.T) {
+	errorMsg := errors.New("Test error")
+
+	instanceIDs := []*string{
+		aws.String("i-1234567890abcdef0"),
+		aws.String("i-1234567890abcdef1"),
+	}
+
+	cwEvent := types.Event{
+		Action:     "stop",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+
+	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
+	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(instanceIDs, nil)
+
+	svcEC2HelperAPI.On("StopEC2Instances", mock.AnythingOfType("[]*string")).Return(nil)
+
+	svcStatusHelperAPI := new(mocks.StatusHelperAPI)
+	svcStatusHelperAPI.On("SetStatusForEnvironment", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(errorMsg)
+
+	base := services{
+		EC2HelperAPI:    svcEC2HelperAPI,
+		StatusHelperAPI: svcStatusHelperAPI,
+	}
+
+	err := base.changeEC2State(cwEvent)
+
+	assert.Error(t, err, "Expected error")
+	assert.Equal(t, errorMsg, err, "Error didn't match given error")
+}
+
+func TestChangeEC2StateStartError(t *testing.T) {
+	errorMsg := errors.New("Test error")
+
+	instanceIDs := []*string{
+		aws.String("i-1234567890abcdef0"),
+		aws.String("i-1234567890abcdef1"),
+	}
+
+	cwEvent := types.Event{
+		Action:     "start",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+
+	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
+	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(instanceIDs, nil)
+
+	svcEC2HelperAPI.On("StartEC2Instances", mock.AnythingOfType("[]*string")).Return(errorMsg)
+
+	base := services{
+		EC2HelperAPI: svcEC2HelperAPI,
+	}
+
+	err := base.changeEC2State(cwEvent)
+
+	assert.Error(t, err, "Expected error")
+	assert.Equal(t, errorMsg, err, "Error didn't match given error")
+}
+
+func TestChangeEC2StateStartStatusError(t *testing.T) {
+	errorMsg := errors.New("Test error")
+
+	instanceIDs := []*string{
+		aws.String("i-1234567890abcdef0"),
+		aws.String("i-1234567890abcdef1"),
+	}
+
+	cwEvent := types.Event{
+		Action:     "start",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+
+	svcEC2HelperAPI := new(mocks.EC2HelperAPI)
+	svcEC2HelperAPI.On("DescribeInstancesForTagsAndAction", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(instanceIDs, nil)
+
+	svcEC2HelperAPI.On("StartEC2Instances", mock.AnythingOfType("[]*string")).Return(nil)
+
+	svcStatusHelperAPI := new(mocks.StatusHelperAPI)
+	svcStatusHelperAPI.On("SetStatusForEnvironment", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(errorMsg)
+
+	base := services{
+		EC2HelperAPI:    svcEC2HelperAPI,
+		StatusHelperAPI: svcStatusHelperAPI,
+	}
+
+	err := base.changeEC2State(cwEvent)
+
+	assert.Error(t, err, "Expected error")
+	assert.Equal(t, errorMsg, err, "Error didn't match given error")
 }
