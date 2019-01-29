8c0bcd9cdfb12c0721dac7150905b9d98f2eedfa
Jan Ritter <jan-ritter@outlook.com>
2019-01-29 21:52:49 +0100

    test: added tests for main rds function

--- main_test.go
+++ main_test.go
@@ -238,3 +238,225 @@ func TestChangeEC2StateStartStatusError(t *testing.T) {
 	assert.Error(t, err, "Expected error")
 	assert.Equal(t, errorMsg, err, "Error didn't match given error")
 }
+
+//
+// RDS Tests
+//
+
+func TestChangeRDSStateStart(t *testing.T) {
+	cwEvent := types.Event{
+		Action:     "start",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+	clusterArn := aws.String("arn:aws:rds:eu-west-1:123456789012:db:mysql-db")
+	clusterStauts := aws.String("available")
+
+	svcRDSHelperAPI := new(mocks.RDSHelperAPI)
+	svcRDSHelperAPI.On("GetRDSClusterForTags", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(clusterArn, clusterStauts, nil)
+	svcRDSHelperAPI.On("StartRDSCluster", mock.AnythingOfType("*string"), mock.AnythingOfType("*string")).Return(true, nil)
+
+	svcStatusHelperAPI := new(mocks.StatusHelperAPI)
+	svcStatusHelperAPI.On("SetStatusForEnvironment", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(nil)
+
+	base := services{
+		RDSHelperAPI:    svcRDSHelperAPI,
+		StatusHelperAPI: svcStatusHelperAPI,
+	}
+
+	err := base.changeRDSState(cwEvent)
+
+	assert.Nil(t, err, "Expected no error")
+	svcRDSHelperAPI.AssertCalled(t, "GetRDSClusterForTags", cwEvent.Repository, cwEvent.Branch)
+	svcRDSHelperAPI.AssertCalled(t, "StartRDSCluster", clusterArn, clusterStauts)
+	svcStatusHelperAPI.AssertCalled(t, "SetStatusForEnvironment", cwEvent.Repository, cwEvent.Branch, "running")
+}
+
+func TestChangeRDSStateStop(t *testing.T) {
+	cwEvent := types.Event{
+		Action:     "stop",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+	clusterArn := aws.String("arn:aws:rds:eu-west-1:123456789012:db:mysql-db")
+	clusterStauts := aws.String("stopped")
+
+	svcRDSHelperAPI := new(mocks.RDSHelperAPI)
+	svcRDSHelperAPI.On("GetRDSClusterForTags", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(clusterArn, clusterStauts, nil)
+	svcRDSHelperAPI.On("StopRDSCluster", mock.AnythingOfType("*string"), mock.AnythingOfType("*string")).Return(true, nil)
+
+	svcStatusHelperAPI := new(mocks.StatusHelperAPI)
+	svcStatusHelperAPI.On("SetStatusForEnvironment", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(nil)
+
+	base := services{
+		RDSHelperAPI:    svcRDSHelperAPI,
+		StatusHelperAPI: svcStatusHelperAPI,
+	}
+
+	err := base.changeRDSState(cwEvent)
+
+	assert.Nil(t, err, "Expected no error")
+	svcRDSHelperAPI.AssertCalled(t, "GetRDSClusterForTags", cwEvent.Repository, cwEvent.Branch)
+	svcRDSHelperAPI.AssertCalled(t, "StopRDSCluster", clusterArn, clusterStauts)
+	svcStatusHelperAPI.AssertCalled(t, "SetStatusForEnvironment", cwEvent.Repository, cwEvent.Branch, "stopped")
+}
+
+func TestChangeRDSStateNoClusterFound(t *testing.T) {
+	cwEvent := types.Event{
+		Action:     "stop",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+	clusterArn := aws.String("")
+	clusterStauts := aws.String("")
+
+	svcRDSHelperAPI := new(mocks.RDSHelperAPI)
+	svcRDSHelperAPI.On("GetRDSClusterForTags", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(clusterArn, clusterStauts, nil)
+
+	base := services{
+		RDSHelperAPI: svcRDSHelperAPI,
+	}
+
+	err := base.changeRDSState(cwEvent)
+
+	assert.Nil(t, err, "Expected no error")
+	svcRDSHelperAPI.AssertCalled(t, "GetRDSClusterForTags", cwEvent.Repository, cwEvent.Branch)
+}
+
+func TestChangeRDSStateGetClusterError(t *testing.T) {
+	cwEvent := types.Event{
+		Action:     "stop",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+	clusterArn := aws.String("")
+	clusterStauts := aws.String("")
+	errorMsg := errors.New("Test error")
+
+	svcRDSHelperAPI := new(mocks.RDSHelperAPI)
+	svcRDSHelperAPI.On("GetRDSClusterForTags", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(clusterArn, clusterStauts, errorMsg)
+
+	base := services{
+		RDSHelperAPI: svcRDSHelperAPI,
+	}
+
+	err := base.changeRDSState(cwEvent)
+
+	assert.Error(t, err, "Expected error")
+	svcRDSHelperAPI.AssertCalled(t, "GetRDSClusterForTags", cwEvent.Repository, cwEvent.Branch)
+	assert.Equal(t, errorMsg, err, "Error didn't match given error")
+}
+
+func TestChangeRDSStateStopClusterError(t *testing.T) {
+	cwEvent := types.Event{
+		Action:     "stop",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+
+	clusterArn := aws.String("arn:aws:rds:eu-west-1:123456789012:db:mysql-db")
+	clusterStauts := aws.String("stopped")
+	errorMsg := errors.New("Test error")
+
+	svcRDSHelperAPI := new(mocks.RDSHelperAPI)
+	svcRDSHelperAPI.On("GetRDSClusterForTags", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(clusterArn, clusterStauts, nil)
+	svcRDSHelperAPI.On("StopRDSCluster", mock.AnythingOfType("*string"), mock.AnythingOfType("*string")).Return(false, errorMsg)
+
+	base := services{
+		RDSHelperAPI: svcRDSHelperAPI,
+	}
+
+	err := base.changeRDSState(cwEvent)
+
+	assert.Error(t, err, "Expected error")
+	svcRDSHelperAPI.AssertCalled(t, "GetRDSClusterForTags", cwEvent.Repository, cwEvent.Branch)
+	svcRDSHelperAPI.AssertCalled(t, "StopRDSCluster", clusterArn, clusterStauts)
+	assert.Equal(t, errorMsg, err, "Error didn't match given error")
+}
+
+func TestChangeRDSStateStartClusterError(t *testing.T) {
+	cwEvent := types.Event{
+		Action:     "start",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+
+	clusterArn := aws.String("arn:aws:rds:eu-west-1:123456789012:db:mysql-db")
+	clusterStauts := aws.String("stopped")
+	errorMsg := errors.New("Test error")
+
+	svcRDSHelperAPI := new(mocks.RDSHelperAPI)
+	svcRDSHelperAPI.On("GetRDSClusterForTags", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(clusterArn, clusterStauts, nil)
+	svcRDSHelperAPI.On("StartRDSCluster", mock.AnythingOfType("*string"), mock.AnythingOfType("*string")).Return(false, errorMsg)
+
+	base := services{
+		RDSHelperAPI: svcRDSHelperAPI,
+	}
+
+	err := base.changeRDSState(cwEvent)
+
+	assert.Error(t, err, "Expected error")
+	svcRDSHelperAPI.AssertCalled(t, "GetRDSClusterForTags", cwEvent.Repository, cwEvent.Branch)
+	svcRDSHelperAPI.AssertCalled(t, "StartRDSCluster", clusterArn, clusterStauts)
+	assert.Equal(t, errorMsg, err, "Error didn't match given error")
+}
+
+func TestChangeRDSStateSetStatusStartError(t *testing.T) {
+	cwEvent := types.Event{
+		Action:     "start",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+	clusterArn := aws.String("arn:aws:rds:eu-west-1:123456789012:db:mysql-db")
+	clusterStauts := aws.String("available")
+	errorMsg := errors.New("Test error")
+
+	svcRDSHelperAPI := new(mocks.RDSHelperAPI)
+	svcRDSHelperAPI.On("GetRDSClusterForTags", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(clusterArn, clusterStauts, nil)
+	svcRDSHelperAPI.On("StartRDSCluster", mock.AnythingOfType("*string"), mock.AnythingOfType("*string")).Return(true, nil)
+
+	svcStatusHelperAPI := new(mocks.StatusHelperAPI)
+	svcStatusHelperAPI.On("SetStatusForEnvironment", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(errorMsg)
+
+	base := services{
+		RDSHelperAPI:    svcRDSHelperAPI,
+		StatusHelperAPI: svcStatusHelperAPI,
+	}
+
+	err := base.changeRDSState(cwEvent)
+
+	assert.Error(t, err, "Expected error")
+	svcRDSHelperAPI.AssertCalled(t, "GetRDSClusterForTags", cwEvent.Repository, cwEvent.Branch)
+	svcRDSHelperAPI.AssertCalled(t, "StartRDSCluster", clusterArn, clusterStauts)
+	svcStatusHelperAPI.AssertCalled(t, "SetStatusForEnvironment", cwEvent.Repository, cwEvent.Branch, "running")
+}
+
+func TestChangeRDSStateSetStatusStopError(t *testing.T) {
+	cwEvent := types.Event{
+		Action:     "stop",
+		Branch:     "branch",
+		Repository: "repo",
+	}
+	clusterArn := aws.String("arn:aws:rds:eu-west-1:123456789012:db:mysql-db")
+	clusterStauts := aws.String("available")
+	errorMsg := errors.New("Test error")
+
+	svcRDSHelperAPI := new(mocks.RDSHelperAPI)
+	svcRDSHelperAPI.On("GetRDSClusterForTags", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(clusterArn, clusterStauts, nil)
+	svcRDSHelperAPI.On("StopRDSCluster", mock.AnythingOfType("*string"), mock.AnythingOfType("*string")).Return(true, nil)
+
+	svcStatusHelperAPI := new(mocks.StatusHelperAPI)
+	svcStatusHelperAPI.On("SetStatusForEnvironment", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(errorMsg)
+
+	base := services{
+		RDSHelperAPI:    svcRDSHelperAPI,
+		StatusHelperAPI: svcStatusHelperAPI,
+	}
+
+	err := base.changeRDSState(cwEvent)
+
+	assert.Error(t, err, "Expected error")
+	svcRDSHelperAPI.AssertCalled(t, "GetRDSClusterForTags", cwEvent.Repository, cwEvent.Branch)
+	svcRDSHelperAPI.AssertCalled(t, "StopRDSCluster", clusterArn, clusterStauts)
+	svcStatusHelperAPI.AssertCalled(t, "SetStatusForEnvironment", cwEvent.Repository, cwEvent.Branch, "stopped")
+}
